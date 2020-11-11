8cdb395be9f37c214df53091e9a1ec1f4b2efeb0
juliana <juliana.cavalaro@hotmail.com>
2020-11-10 22:18:17 -0300

    minhaLojaDeGames em Progresso

--- minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/controller/CategoriaController.java
+++ minhaLojaDeGames/src/main/java/org/generation/minhaLojaDeGames/controller/CategoriaController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,6 +41,10 @@ public class CategoriaController {
 	@PostMapping
 	public ResponseEntity<Categoria> post (@RequestBody Categoria categoria){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(categoria));
+	}
+	@PutMapping
+	public ResponseEntity<Categoria> put (@RequestBody Categoria categoria){
+		return ResponseEntity.status(HttpStatus.OK).body(repository.save(categoria));
 	}
 	
 
